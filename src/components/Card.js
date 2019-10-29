d27ec9115489f711228d765d28378ee9fbd4994d
Yr0z <y.cocheteau@gmail.com>
2019-10-29 09:55:46 +0100

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import './Card.css';
+
+const Card = () => {
+    return (
+    
+    );
+}
+
+export default Card;
---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Main.css'
+
+function Main(){
+    return(
+
+        <div className="Container">
+    
+            <div className="carre">
+
+                <div className="Card"></div>
+
+            </div>
+		
+        </div> 
+ 
+    )
+}
+
+
+
+export default Main;
