877b672bb47bae1da869e4a9abb6b225e5520b75
Simon TSCHAN <tschan.omis@gmail.com>
2019-11-04 09:59:04 +0100

    index done

--- src/components/DataAnalyse.js
+++ src/components/DataAnalyse.js
@@ -3,7 +3,7 @@ import React from 'react'
 function DataAnalyse({ data }) {
   console.log('DATA analyse')
   const SO2 = data.SO2.value_upm;
-  const NO2 = data.NO2.value_upm;
+  const NO2 = (data.NO2.value_upm);
   const O3 = data.O3.value_upm;
   const PM10 = data.PM10.value_upm;
 
@@ -12,103 +12,105 @@ function DataAnalyse({ data }) {
   //SO2
   let SO2Index = 0
   switch (true) {
-    case 0 < SO2 < 39:
+    case SO2 < 39:
       SO2Index = 1;
       break;
-    case 40 < SO2 < 79:
+    case SO2 < 79:
       SO2Index = 2;
       break;
-    case 80 < SO2 < 119:
+    case SO2 < 119:
       SO2Index = 3;
       break;
-    case 120 < SO2 < 159:
+    case SO2 < 159:
       SO2Index = 4;
       break;     
-    case 160 < SO2 < 199:
+    case SO2 < 199:
       SO2Index = 5;
       break;
-    case 200< SO2 < 249:
+    case SO2 < 249:
       SO2Index = 6;
       break;
-    case 250 < SO2 < 299:
+    case SO2 < 299:
       SO2Index = 7;
       break;
-    case 300 < SO2 < 399:
+    case SO2 < 399:
       SO2Index = 8;
       break;
-    case 400 < SO2 < 499:
+    case SO2 < 499:
       SO2Index = 9;
       break;    
-    case 500 <= SO2:
+    case SO2 >= 500:
       SO2Index = 10;
       break;
     default:
       console.log('Sorry');
   }
+  
   //NO2
     let NO2Index = 0
     switch (true) {
-      case 0 < NO2 < 29:
+      case NO2 < 29:
         NO2Index = 1;
         break;
-      case 30 < NO2 < 54:
+      case NO2 < 54:
         NO2Index = 2;
         break;
-      case 55 < NO2 < 84:
+      case NO2 < 84:
         NO2Index = 3;
         break;
-      case 85 < NO2 < 109:
+      case NO2 < 109:
         NO2Index = 4;
         break;     
-      case 110 < NO2 < 134:
+      case NO2 < 134:
         NO2Index = 5;
         break;
-      case 135 < NO2 < 164:
+      case NO2 < 164:
         NO2Index = 6;
         break;
-      case 165 < NO2 < 199:
+      case NO2 < 199:
         NO2Index = 7;
         break;
-      case 200 < NO2 < 270:
+      case NO2 < 270:
         NO2Index = 8;
         break;
-      case 275 < NO2 < 399:
+      case NO2 < 399:
         NO2Index = 9;
         break;    
-      case 400 <= NO2:
+      case NO2 >= 400:
         NO2Index = 10;
         break;
       default:
         console.log('Sorry');
     }
+
     //O3
-    let O3Index = 0
+    let O3Index;
     switch (true) {
-      case 0 < O3 < 29:
+      case O3 < 29:
         O3Index = 1;
         break;
-      case 30 < O3 < 54:
+      case O3 < 54:
         O3Index = 2;
         break;
-      case 55 < O3 < 84:
+      case O3 < 84:
         O3Index = 3;
         break;
-      case 80 < O3 < 109:
+      case O3 < 109:
         O3Index = 4;
         break;     
-      case 105 < O3 < 134:
+      case O3 < 134:
         O3Index = 5;
         break;
-      case 130 < O3 < 164:
+      case O3 < 164:
         O3Index = 6;
         break;
-      case 150 < O3 < 199:
+      case O3 < 199:
         O3Index = 7;
         break;
-      case 180 < O3 < 270:
+      case O3 < 270:
         O3Index = 8;
         break;
-      case 210 < O3 < 399:
+      case O3 < 399:
         O3Index = 9;
         break;    
       case 240 <= O3:
@@ -117,13 +119,14 @@ function DataAnalyse({ data }) {
       default:
         console.log('Sorry');
     }
+    
     //PM10
     let PM10Index = 0
     switch (true) {
-      case 0 < PM10 < 6:
+      case PM10 < 6:
         PM10Index = 1;
         break;
-      case 7 < PM10 < 13:
+      case PM10 < 13:
         PM10Index = 2;
         break;
       case 14 < PM10 < 20:
@@ -135,19 +138,19 @@ function DataAnalyse({ data }) {
       case 28 < PM10 < 34:
         PM10Index = 5;
         break;
-      case 35 < PM10 < 41:
+      case PM10 < 41:
         PM10Index = 6;
         break;
-      case 42 < PM10 < 49:
+      case PM10 < 49:
         PM10Index = 7;
         break;
-      case 50 < PM10 < 64:
+      case PM10 < 64:
         PM10Index = 8;
         break;
-      case 65 < PM10 < 79:
+      case PM10 < 79:
         PM10Index = 9;
         break;    
-      case 80 <= PM10:
+      case PM10 >= 80:
         PM10Index = 10;
         break;
       default:
@@ -157,6 +160,7 @@ function DataAnalyse({ data }) {
     resultTab.push(SO2Index, NO2Index, O3Index, PM10Index);
     console.log('tab: ', resultTab);
     const resultIndex = Math.max(...resultTab);
+    //result 
     console.log('index: ', resultIndex);
     
   return (
--- src/components/LocationData.js
+++ src/components/LocationData.js
@@ -1,6 +1,8 @@
 const LocationData = {
-  latitude: '48.85',
-  longitude: '2.294',
+  //latitude: '48.85', longitude: '2.294',
+  latitude: '45.183471',  longitude: '5.71728',
+  //19.408449, -99.147709
+  //34.036661, -118.452287
 }
-
+//5.71728, 45.183471
 export default LocationData
