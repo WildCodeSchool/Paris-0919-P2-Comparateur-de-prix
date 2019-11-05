a75bf58884148f9ccf4ca7bf4e3ba5c5736818f7
Lilybn <liliabentahar15@gmail.com>
2019-11-05 15:37:03 +0100

    WIP

--- src/Components/Bouton.jsx
+++ src/Components/Bouton.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import '../App'
 import './Bouton.css'
 
 function Bouton() {
     return (
-        <div>
-<section>
-  <nav> <button class="round-button" type="button" id="toggle-button" aria-label="open/close navigation"></button>
-<ul>
-    <li>Menu</li>
-    <li>FAQ</li>
-    <li>Contact me BG</li>
+
+   
+       <div>
+  <section>
+  <figure role="navigation"> <button class="round-button" type="button" id="toggle-button" aria-label="open/close navigation"></button>
+  <a className="ball"> ok ok ok </a>
+
+
+<ul className="hover">
+    <a href=""><li> Menu</li></a>
+    <a href=""><li> Menu</li></a>
+    <a href=""><li> Menu</li></a>
 </ul>
 
-</nav>
-</section>
-  
+
+</figure>
+
+</section> 
         </div>
     );
 }
+
+
 export default Bouton;
