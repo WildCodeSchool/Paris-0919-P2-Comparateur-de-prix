75a0706c8b0307f14626571a9af46a194a11a380
Cange <william.mp@sfr.fr>
2019-11-06 10:37:01 +0100

    styleinput

--- src/components/AutoCompleteText.js
+++ src/components/AutoCompleteText.js
@@ -9,10 +9,16 @@ import './AutoComplete.css'
   }
 
   items = [
-    '15 rue de poissy',
-    '40 rue du poulet',
-    '2 rue de la chèvre',
-    '40 rue de chepaskoi',
+    'WildCodeSchool, 11 rue de Poissy, 75005, Paris',
+    'Google France, 8 rue Londres, 75009, Paris ',
+    'Societe General, 3 rue Rébeval, 75019, Paris',  
+    'HSBC Paris Auber,10 rue Auber, 75009 Paris',
+    'Infobat, 34 rue de Picpus, 75012 PARIS',
+    'Ionis X, 2 rue des quatre fils, 75003 Paris',
+    'Manwork, 140 Rue de rivoli, 75001 Paris',
+    'Chauvin Arnoux, 190 Rue Championnet, 75018 Paris',
+    'LTD International, 43 Rue du FG Montmartre, 75009 Paris',
+    'Baselec Entreprise, 56 Boulevard Diderot, 75012 Paris',
   ]
   
   onTextChanged = e => {
