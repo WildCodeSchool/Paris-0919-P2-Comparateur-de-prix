b25aae52f8f381b07b3797ff558c56f193e393f5
Simon TSCHAN <tschan.omis@gmail.com>
2019-11-06 16:16:48 +0100

    test index bar

--- src/screen/Result-page.js
+++ src/screen/Result-page.js
@@ -3,13 +3,15 @@ import '../App'
 import './Result-page.css'
 
 function ResultPage() {
-  console.log("hello result page")
+  console.log("hello result page");
+  const test = (Math.floor(Math.random()*100));
+  console.log(test);
   return (
     <div className="all">
       <div className="container">
         <h1>TEST</h1>
         <div className="Note">
-          <div className="cursor"></div>
+          <div className="cursor" style={{marginLeft: test + '%'}}></div>
         </div>
       </div>
     </div>
