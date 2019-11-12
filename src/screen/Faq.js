45d489b946c6c11d93bd4449334cf7f0c440b73a
Vincent Kouoï <vincentkouoi@gmail.com>
2019-11-12 15:48:08 +0100

    Corrected questions/answers from Faq

--- src/screen/Faq.js
+++ src/screen/Faq.js
@@ -4,7 +4,17 @@ import './Faq.css';
 const Faq = () => {
 	return (
 		<div>
-			<h1>
+            <h1>Qui sommes nous ?</h1>
+            <p>Go Green est une application qui veille à la santé des employés d'une entreprise en mesurant le taux de pollution qui circule à paris.</p>
+            <h1>Comment utilise t-on cette application</h1>
+            <p>Vous sélectionnez dans les 2 barres vides, vous insérez vos lieux de reésidences et de destinations et vous appuyez 'Obtenir la pollution'.</p>
+            <h1>Pour quel but avez vous construit cette applicaton ?</h1>
+            <p>Les utilisateurs souffrant des maladies comme le cancer au poumon pourraient prendre de meilleures mesures face à la pollution.</p>
+            <h1>Quelles sortes de mesures les utilisateurs peuvent employer contre la pollution ?</h1>
+            <p>Par exemple, ils pourraient rester chez eux et faire du télétravail.</p>
+            <h1>Qu'est ce que cet applicaton mesure ?</h1>
+            <p>Cette application mesure le taux de pollution en France. Que ce soit à Lyon, Rouen, Paris etc.</p>
+			{/* <h1>
 				<li>Qui sommes nous?</li>
 				<p>Go Green est une application qui veille à la santé des employés d'une entreprise en mesurant le taux de pollution qui circule à paris.</p>
 			</h1>
@@ -23,7 +33,7 @@ const Faq = () => {
 			<h5>
 				<li>Est ce que cela applique seulement aux employés ?</li>
 				<p>Non, ça s'applique aussi à leur employeur qui peut conseiller à leurs employés de rester chez eux à faire du télétravail.</p>
-			</h5>
+			</h5> */}
 		</div>
 		
 	)
