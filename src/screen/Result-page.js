3c7e0b5ebb3599dca80e7e624f0f4a6025863fc9
Simon TSCHAN <tschan.omis@gmail.com>
2019-11-13 11:54:10 +0100

    scale and info done

--- src/screen/Result-page.js
+++ src/screen/Result-page.js
@@ -6,26 +6,38 @@ import DisplayData from '../components/DisplayData';
 class ResultPage extends React.Component {
   state = {
     index: null,
+    hide: true,
   }
 
   handleIndex = (data) => {
     this.setState({index: data})
   }
 
+  getMoreInfo = () => {
+    let newHide = !this.state.hide
+    this.setState({hide: newHide})  
+  }
+
   render(){
   console.log("result: ", this.props.data);
-  //const test = (Math.floor(Math.random()*100));
   const test = (this.state.index * 10)
   console.log(test);
   return (
     <div className="all">
       <div className="container">
-        <h1>TEST</h1>
+        <h1>INDICE AIR</h1>
         <div className="Note">
           <div className="cursor" style={{marginLeft: test + '%'}}></div>
         </div>
-        <div>
-          <DisplayData data={this.props.data}/>
+        <div className="info">
+          <button className="moreInfo" onClick={this.getMoreInfo}>
+            {this.state.hide ? '+' : '-'}  
+          </button>
+          <div>
+            <div className="moreInfoDisplay" style={this.state.hide ? {display: 'none'} : {display: 'block'}}>
+              <DisplayData data={this.props.data}/>
+            </div>
+          </div>
           <DataAnalyse data={this.props.data} index={this.handleIndex}/>
         </div>
       </div>
